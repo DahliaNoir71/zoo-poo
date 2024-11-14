b092ad8335e0961628cabea924f11a077915149c
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-14 16:58:16 +0100

    Refactoring

--- Classes/animal.py
+++ Classes/animal.py
@@ -1,23 +1,26 @@
 class Animal:
-    name = ''
-    species = ''
-    preys = []
+    nom = ''
+    espece = ''
+    proies = []  # List of animals this animal preys on.
+
 
     def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.preys = []
+        self.nom = name
+        self.espece = species
+        self.proies = []
 
 
 class Lion(Animal):
-    species = "Lion"
+    espece = "Lion"
+    proies = ["Gazelle"]  # Lions are known to prey on elephants and wildebeests.
+
 
     def __init__(self, name):
         super().__init__(name, self.species)
 
 
 class Gazelle(Animal):
-    species = "Gazelle"
+    espece = "Gazelle"
 
     def __init__(self, name):
         super().__init__(name, self.species)
